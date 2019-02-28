ebe15b3eebfee19fc6ea5cba9db14377ff696a6e
Stacey Chin <sschin@ucsd.edu>
2019-02-28 00:06:55 -0800

    thirdpartyAPI lab

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,12 @@
 function initMap() {
-	// add your code here
+	L.mapquest.key = 'WXBfAdOBUf1vIAehdxj2ltt3e5r8TZ6B';
+
+	var map = L.mapquest.map('map', {
+		center: [32.879353, -117.2311],
+		layers: L.mapquest.tileLayer('map'),
+		zoom: 12,
+		zoomControl: false
+	});
+
+	L.marker([32.88, -117.236]).addTo(map);
 }
--- routes/rsvp.js
+++ routes/rsvp.js
@@ -8,3 +8,16 @@ var data = {
 exports.adminView = function(request, response){
   response.render('rsvp', data);
 };
+
+exports.addRSVP = function(request, response){
+	// console.log(request);
+
+  var rsvpEmail = request.body.rsvpEmail;
+
+  console.log(rsvpEmail);
+
+  data.rsvp.push(rsvpEmail);
+
+  response.send(rsvpEmail);
+  
+};
